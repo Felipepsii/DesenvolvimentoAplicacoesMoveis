70ad61ad1a912f1022f69a67d25f9b04d2e06908
Felipe O. Gomes <69019358+Felipepsii@users.noreply.github.com>
2022-08-19 20:58:55 -0300

    1.0v

--- Aula02/app/src/main/java/com/br/up/sortedodia/MainActivity.java
+++ Aula02/app/src/main/java/com/br/up/sortedodia/MainActivity.java
@@ -7,11 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTitle;
 
     private Button buttonAction;
+    private ArrayList<String> messages = new ArrayList<>();
 
 
 
@@ -25,12 +29,26 @@ public class MainActivity extends AppCompatActivity {
         buttonAction= findViewById(R.id.button_action);
 
 
+        messages.add("Criar o Whatszap2");
+        messages.add("Criar o Whatszap3");
+        messages.add("Criar um partido Marxista-Leninista");
+        messages.add("Criar o Whatszap4");
+
+
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                textViewTitle.setText("É o zap zap zap zap zap zap zap");
+                Random random = new Random();
+                int index = random.nextInt(messages.size());
+                String message = messages.get(index);
+
+                textViewTitle.setText((message));
+
+
+
+//                textViewTitle.setText("Eu faço parte do partido comunista chines");
 
 
             }
