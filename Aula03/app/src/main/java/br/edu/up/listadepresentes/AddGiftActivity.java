36c1462952ed25e0207d15789ff8d7fbcf87539f
Felipe O. Gomes <69019358+Felipepsii@users.noreply.github.com>
2022-08-19 22:10:25 -0300

    Aula3 0.1v

---
+++ Aula03/app/src/main/java/br/edu/up/listadepresentes/AddGiftActivity.java
@@ -0,0 +1,14 @@
+package br.edu.up.listadepresentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddGiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gift);
+    }
+}
